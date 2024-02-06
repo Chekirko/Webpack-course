07be44a05bfc9849ddac63934906c461f1774acb
chekirko <victorche59@gmail.com>
2024-02-06 19:31:51 +0200

    add css loaders

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 type Mode = 'production' | 'development';
@@ -12,6 +13,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const isDev = env.mode === 'development';
+  const isProd = env.mode === 'production';
 
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
@@ -22,11 +24,28 @@ export default (env: EnvVariables) => {
       clean: true,
     },
     plugins: [
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[contenthash:8].css',
+          chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
       isDev && new webpack.ProgressPlugin(),
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
     ].filter(Boolean),
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
